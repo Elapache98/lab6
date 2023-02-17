1372074032161fd377e63a9098b744ff8e768421
Ade <mymail@gmail.comadeobayomi@gmail.com>
2023-02-17 02:45:49 -0500

    comments for logic

--- main.js
+++ main.js
@@ -33,12 +33,22 @@ svg.append("rect")
     .attr("height", svgHeight - margin * 2);
 
 let dataset = [
+    /*** 
+     x= daily observation of happiness levels on a scale of 0-5 with 0 being sad and 5 being happy
+     y= minutes spent on Youtube/day
+     ***/
+    /*** 11/02/23 ***/
     { x: 3, y: 10 },
+    /*** 12/02/23 ***/
     { x: 4.5, y: 237 },
+    /*** 13/02/23 ***/
     { x: 3, y: 90 },
-    { x: 4, y: 214 },
+    /*** 14/02/23 ***/
+    { x: 3, y: 214 },
+    /*** 15/02/23 ***/
     { x: 3, y: 102 },
-    { x: 3.5, y: 165 }
+    /*** 16/02/23 ***/
+    { x: 4, y: 165 }
 ];
 
 let happinessRange = d3.scaleLinear()
@@ -60,6 +70,19 @@ circles.attr("r", 5)
     .attr("cy", function (value) {
         return minutesOnYoutube(value.y);
     })
+    .attr("fill", function (value)
+    /****  the function takes into consideration the value of the x axis to depict what color the circle would be
+    grey = less than 3
+    orange = 3
+    green = more than 3
+    (0-sad & grey circles  3- average & orange circles 5-very happy & green circles)
+    this provides an easier way to visually discern how my my mood and my increase or decrease in youtube consumption are correlated****/ {
+        if (value.x < 3) {
+            return "grey";
+        } else if (value.x > 3) {
+            return "green";
+        } else { return "orange"; }
+    });
 
 /**** label the axes ****/
 let xAxisLabel = svg.append("text")
@@ -81,13 +104,16 @@ let originLabel = svg.append("text")
     .attr("x", margin)
     .attr("y", svgHeight - (margin / 2))
     .attr("text-anchor", "middle")
-    .text("0,0");
+    .text("0");
 
+/* labels the max value of coordinates on the X axis */
 let xAxisMaxValue = svg.append("text")
     .attr("x", svgWidth - margin)
     .attr("y", svgHeight - (margin / 2))
     .attr("text-anchor", "end")
     .text("5");
+
+/* labels the max value of coordinates on the Y axis */
 
 let yAxisMaxValue = svg.append("text")
     .attr("x", -margin)
